38f2e63e89a0dc511f9bcc4f727aa7a9db820d83
Javier Ruiz <javierruizjr15@gmail.com>
2023-08-30 15:22:24 -0700

    Logo Adjustment 2.0

--- src/components/logoleft.js
+++ src/components/logoleft.js
@@ -6,12 +6,15 @@ const LogoLeft = () => {
     
     return (
         <>
-                <div className="column flex-top-left">
-                    <img 
+                <div className="column">
+                    
+                    <div className="flex-top-left"> 
+                        <img 
                         src={deltacoreslogo} 
                         alt="delta cores logo" 
                         style={{ width: "72px", height: "72px" }} 
                         /> 
+                    </div>
                 </div>
         </>
     )
