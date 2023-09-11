4693e7eaa239340c59f2319785e6cf3e312bea71
Javier Ruiz <javierruizjr15@gmail.com>
2023-09-10 19:43:42 -0700

    Re-intro

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,21 +3,21 @@ import '../index.css';
 import LogoLeft from "../components/logoleft";
 import MenuItems from "../components/menuItems";
 import NavRight from "../components/navRight";
-// import Platonics from "../components/platonics";
-import Polygon from "../components/polygon";
+import Platonics from "../components/platonics";
+// import Polygon from "../components/polygon";
 
 const Home = () => {
     
         
     return (
         <>
-            <Polygon>
+            <Platonics>
                 <div className="home-page">
                     <LogoLeft />
                     <MenuItems />
                     <NavRight />
                 </div>
-            </Polygon>
+            </Platonics>
         </>
 
     );
