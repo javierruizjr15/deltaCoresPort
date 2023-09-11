16e5cd651dec109cfc24d7a298cde30e51a54461
Javier Ruiz <javierruizjr15@gmail.com>
2023-09-11 11:10:11 -0700

    button test

--- src/components/logoleft.js
+++ src/components/logoleft.js
@@ -8,11 +8,13 @@ const LogoLeft = () => {
         <>
                 <div className="column">
                     <div className="flex-top-left"> 
-                        <img 
-                        src={deltacoreslogo} 
-                        alt="delta cores logo" 
-                        style={{width: "70px", height: "100px" }} 
-                        /> 
+                        <div>
+                            <img 
+                            src={deltacoreslogo} 
+                            alt="delta cores logo" 
+                            style={{width: "70px", height: "100px" }} 
+                            /> 
+                        </div>
                     </div>
                 </div>
         </>
